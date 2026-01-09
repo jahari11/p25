412629676079f7b056f6ae05078c1990bcd0b657
Your Name <jahari.crawford.young@gmail.com>
2026-01-09 11:39:20 -0500

    pp26

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,37 +1,35 @@
-import {FaInstagram} from 'react-icons/fa';
+
 import { MdEmail } from 'react-icons/md';
 import Image from 'next/image';
-import { Logo } from '../assets/25logo.png';
+import Logo from '../assets/25logo.png';
 import { useState } from 'react';
 import ContactForm from './ContactForm';
 
 export default function Footer() {
-    const [showContactForm, setShowContactForm] = useState(false)
-    return (
-        <div className="bg-black px-4 py-16 flex flex-col items-center">
-            <span className="text-white md:text-[30px] uppercase font-bold">Phoenix project 2025</span>
-            <Image src={Logo} width={300} />
-            <span className="text-red-600 font-bold text-center uppercase md:text-[30px]">Lets talk: <a href='mailto:phoenixproject4you@gmail.com'>phoenixproject4you@gmail.com</a></span>
-            <div className='flex gap-2'>
-        <a href='https://www.instagram.com/phoenixpro5ect'>
-        <FaInstagram className='text-white text-2xl hover:text-red-600' />
-        </a>
-        <MdEmail className='text-white text-2xl cursor-pointer hover:text-red-600' onClick={()=>setShowContactForm(!showContactForm)} />
-        
-            </div>
-            {showContactForm && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                      <div className="bg-white dark:bg-black p-6 rounded-lg shadow-lg relative w-[90%] max-w-md">
-                        <button
-                          className="absolute top-2 right-2 text-red-600 hover:text-black font-black"
-                          onClick={() => setShowContactForm(false)}
-                        >
-                         X {/* Close button */}
-                        </button>
-                        <ContactForm />
-                      </div>
-                    </div>
-                  )}
+  const [showContactForm, setShowContactForm] = useState(false)
+  return (
+    <div className="bg-black px-4 py-16 flex flex-col items-center">
+      <span className="text-white md:text-[30px] uppercase font-bold">Phoenix project 2026</span>
+      <Image src={Logo} width={300} />
+      <span className="text-red-600 font-bold text-center uppercase md:text-[30px]">Lets talk: <a href='mailto:phoenixproject4you@gmail.com'>phoenixproject4you@gmail.com</a></span>
+      <div className='flex gap-2'>
+
+        <MdEmail className='text-white text-2xl cursor-pointer hover:text-red-600' onClick={() => setShowContactForm(!showContactForm)} />
+
+      </div>
+      {showContactForm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white dark:bg-black p-6 rounded-lg shadow-lg relative w-[90%] max-w-md">
+            <button
+              className="absolute top-2 right-2 text-red-600 hover:text-black font-black"
+              onClick={() => setShowContactForm(false)}
+            >
+              X {/* Close button */}
+            </button>
+            <ContactForm />
+          </div>
         </div>
-    )
+      )}
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,29 @@ import Logo from '../assets/25logo.png'
 import WebIcon from '../assets/webicon.png'
 import Image from 'next/image'
 import Timeline from '@/components/Timeline'
-import {FaInstagram} from 'react-icons/fa'
+
 import { MdEmail } from "react-icons/md";
 import { Card } from '@/components/Card'
 import About from '@/components/About'
 import ContactForm from '@/components/ContactForm'
 import { useState } from 'react';
-export default function Home () {
+export default function Home() {
 
   const [showContactForm, setShowContactForm] = useState(false);
-  
-  return (
-  <div className='min-h-screen bg-black px-4 md:px-6 lg:px-8 '>
-    <header className='flex items-center justify-between px-4 py-4 mx-auto'>
-      <div className='flex items-center '>
-        <Image src={WebIcon} className='hidden md:block' />
-      </div>
-      <div className='flex gap-4'>
-      <a href='https://www.instagram.com/phoenixpro5ect'>
-        <FaInstagram className='text-white hover:text-red-600 text-2xl' />
-      </a>
-      <MdEmail className='text-white hover:text-red-600 cursor-pointer text-2xl' onClick={()=> setShowContactForm(!showContactForm)} />
-      </div>
-    </header>
 
-    {showContactForm && (
+  return (
+    <div className='min-h-screen bg-black px-4 md:px-6 lg:px-8 '>
+      <header className='flex items-center justify-between px-4 py-4 mx-auto'>
+        <div className='flex items-center '>
+          <Image src={WebIcon} className='hidden md:block' />
+        </div>
+        <div className='flex gap-4'>
+
+          <MdEmail className='text-white hover:text-red-600 cursor-pointer text-2xl' onClick={() => setShowContactForm(!showContactForm)} />
+        </div>
+      </header>
+
+      {showContactForm && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white dark:bg-black p-6 rounded-lg shadow-lg relative w-[90%] max-w-md">
             <button
@@ -39,44 +37,44 @@ export default function Home () {
           </div>
         </div>
       )}
-    {/*Hero Content */}
-    <div className='min-h-screen px-4'>
-      <div className='max-w-4xl mx-auto text-center pt-16 pb-24'>
-        <div className='flex justify-center mb-4'>
-          <Image src={Logo} />
+      {/*Hero Content */}
+      <div className='min-h-screen px-4'>
+        <div className='max-w-4xl mx-auto text-center pt-16 pb-24'>
+          <div className='flex justify-center mb-4'>
+            <Image src={Logo} />
+          </div>
+          <h1 className=' text-6xl md:text-8xl font-bold mb-2 text-red-600 uppercase text-center'>Phoenix Project</h1>
+          <h2 className='text-4xl sm:text-5xl font-bold mb-6 tracking-tight text-white uppercase'>I think, therefore I choose Phoenix Project</h2>
+          <p className='text-xl text-white uppercase font-thin'>We craft digital experiences that spark growth, automate success, and position your business for the future.</p>
         </div>
-      <h1 className=' text-6xl md:text-8xl font-bold mb-2 text-red-600 uppercase text-center'>Phoenix Project</h1>
-      <h2 className='text-4xl sm:text-5xl font-bold mb-6 tracking-tight text-white uppercase'>I think, therefore I choose Phoenix Project</h2>
-      <p className='text-xl text-white uppercase font-thin'>We craft digital experiences that spark growth, automate success, and position your business for the future.</p>
+      </div>
+
+      {/*Page one Content */}
+      <div className='relative min-h-screen snap-start bg-black'>
+        <div>
+          <h1 className='text-6xl md:text-[280px] leading-none font-bold tracking-tight text-red-600'>
+            OUR SERVICES
+          </h1>
+        </div>
+        <Card />
+      </div>
+
+      {/*Page two Content */}
+      <div className='min-h-screen relative bg-black'>
+
+        <h1 className=" text-6xl text-red-600 font-bold  md:text-[280px] md:whitespace-nowrap text-right leading-none  ">
+          OUR PROCESS
+        </h1>
+
+        <Timeline />
+      </div>
+
+      {/*Page 3 Content */}
+      <div className='min-h-screen bg-black'>
+        <h1 className='text-red-600 font-bold text-6xl md:text-[280px] leading-none whitespace-nowrap uppercase'>About Us</h1>
+        <About />
       </div>
     </div>
-
-    {/*Page one Content */}
-  <div className='relative min-h-screen snap-start bg-black'>
-      <div>
-        <h1 className='text-6xl md:text-[280px] leading-none font-bold tracking-tight text-red-600'>
-          OUR SERVICES
-        </h1>
-      </div>
-      <Card />
-      </div>
-
-{/*Page two Content */}
-  <div className='min-h-screen relative bg-black'>
-  
-  <h1 className=" text-6xl text-red-600 font-bold  md:text-[280px] md:whitespace-nowrap text-right leading-none  ">
-    OUR PROCESS
-  </h1>
-  
-  <Timeline />
-  </div>
-
-  {/*Page 3 Content */}
-  <div className='min-h-screen bg-black'>
-    <h1 className='text-red-600 font-bold text-6xl md:text-[280px] leading-none whitespace-nowrap uppercase'>About Us</h1>
-  <About />
-  </div>
-  </div>
 
   )
 }
